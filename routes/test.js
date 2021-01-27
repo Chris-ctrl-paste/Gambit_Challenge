663fe0690acebfbce99a22d36999a5d97ff0f616
Chris <christian.osterback@gmail.com>
2021-01-27 20:47:09 +0200

    I am rendering something! I feel there is 50 better ways to actually do this. but shit is displaying and that is what matters, may or may not try to code it in a different way

---
+++ routes/test.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const router = express.Router()
+const fs = require('fs');
+
+
+
+router.get("/test", function(req, res){ 
+
+
+
+    res.render('test',{
+        
+        
+     })
+
+
+})
